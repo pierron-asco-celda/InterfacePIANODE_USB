83d6025656e79942f5b72c55fee1da2f3c019483
Frédéric Thomé <fthome@pierron.fr>
2018-04-18 19:29:54 +0200

    V0.1.0.2

--- InterfacePIANODE_USB/My Project/AssemblyInfo.vb
+++ InterfacePIANODE_USB/My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' en utilisant '*', comme indiqué ci-dessous :
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("0.1.0.1")>
-<Assembly: AssemblyFileVersion("0.1.0.1")>
+<Assembly: AssemblyVersion("0.1.0.2")>
+<Assembly: AssemblyFileVersion("0.1.0.2")>
 <Assembly: NeutralResourcesLanguage("fr")>
